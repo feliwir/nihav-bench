0150d94bc3ee19cde6c5201e1c33343c9ce77c94
Stephan Vedder <vedder@mbits.info>
2021-11-18 16:54:45 +0100

    Fix build to use new repo

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ rm -rf $SCRIPT_DIR/nihav
 git clone git://git.nihav.org/nihav.git nihav
 git clone git://git.nihav.org/nihav-encoder.git  nihav/nihav-encoder
 
-export RUSTFLAGS="-C target-feature=+sse4.2,+avx2"
+rm -rf $SCRIPT_DIR/nihav-fb
+git clone git@github.com:feliwir/nihav-fb.git nihav-fb
+git clone git://git.nihav.org/nihav-encoder.git  nihav-fb/nihav-encoder
+
 # Apply regular patches required for proper EA VP6 workflow
 cd $SCRIPT_DIR/nihav
 find ../patches/ -name *.patch -print0 | xargs -0 git am
@@ -15,12 +18,11 @@ cd $SCRIPT_DIR/nihav/nihav-encoder
 cargo build --release
 cp $SCRIPT_DIR/nihav/nihav-encoder/target/release/nihav-encoder $SCRIPT_DIR/nihav-encoder-base
 
-# Apply patches that improve performance
-cd $SCRIPT_DIR/nihav
-find $SCRIPT_DIR/improved/nihav -name *.patch -print0 | xargs -0 git am
-cd $SCRIPT_DIR/nihav/nihav-encoder
-find $SCRIPT_DIR/improved/nihav-encoder -name *.patch -print0 | xargs -0 git am
+# Apply regular patches required for proper EA VP6 workflow
+cd $SCRIPT_DIR/nihav-fb
+find ../patches/ -name *.patch -print0 | xargs -0 git am
 
-cd $SCRIPT_DIR/nihav/nihav-encoder
+# Build an optimized encoder
+cd $SCRIPT_DIR/nihav-fb/nihav-encoder
 cargo build --release
-cp $SCRIPT_DIR/nihav/nihav-encoder/target/release/nihav-encoder $SCRIPT_DIR/nihav-encoder-opt
+cp $SCRIPT_DIR/nihav-fb/nihav-encoder/target/release/nihav-encoder $SCRIPT_DIR/nihav-encoder-opt
